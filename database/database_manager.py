a0458659389be1d3f2323fdcd47427271329f7dc
mikiminachx <dezmeko@icloud.com>
2024-01-18 15:38:26 +0800

    Updated database manager

--- database/database_manager.py
+++ database/database_manager.py
@@ -1,36 +1,41 @@
 import sqlite3
 
-# Connect to SQLite database
-def connect_db():
-    conn = sqlite3.connect('password.db')
-    return conn
+class DatabaseManager:
+    def __init__(self, db_path='password.db'):
+        self.db_path = db_path
+        self.conn = self.create_connection()
+        self.create_table()
 
-# Create table for storing passwords
-def create_table(conn):
-    conn.execute('''
-    CREATE TABLE IF NOT EXISTS passwords(
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                service TEXT NOT NULL,
-                encrypted_password TEXT NOT NULL
-    );
-    ''')
+    # Connect to SQLite database
+    def create_connection(self):
+        return sqlite3.connect(self.db_path)
 
-    conn.commit()
+    # Create table for storing passwords
+    def create_table(self):
+        with self.conn:
+            self.conn.execute('''
+            CREATE TABLE IF NOT EXISTS passwords(
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        service TEXT NOT NULL,
+                        encrypted_password TEXT NOT NULL
+            );
+            ''')
 
-# Inserting password
-def insert_password(conn, service, encrypted_password):
-    conn.execute(conn.execute("INSERT INTO passwords (service, encrypted_password) VALUES (?, ?)", (service, encrypted_password)))
-    conn.commit()
+    # Inserting password
+    def insert_password(self, service, encrypted_password):
+        with self.conn:
+            cursor = self.conn.execute("INSERT INTO passwords (service, encrypted_password) VALUES (?, ?)", (service, encrypted_password))
 
-# Retrieving password
-def retrieve_password(conn, service):
-    cursor = conn.execute("SELECT encrypted_password FROM password WHERE service=?", (service,))
-    return cursor.fetchone()
+    # Retrieving password
+    def retrieve_password(self, service):
+        cursor = self.conn.execute("SELECT encrypted_password FROM password WHERE service=?", (service,))
+        row = cursor.fetchone()
+        return row[0] if row else None
 
 # Testing
 if __name__ == "__main__":
-    conn = connect_db()
-    create_table(conn)
-    conn.close()
+    db_manager = DatabaseManager()
+    db_manager.insert_password("example_password", "encrypted_example_password")
+    print(db_manager.retrieve_password("example_service"))
 
 
