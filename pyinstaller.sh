2f093787c03a1f3cc28957dfdbc063946196cda2
mikiminachx <dezmeko@icloud.com>
2024-01-23 13:38:53 +0800

    Deleted unnecessary files

--- pyinstaller.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-pyinstaller --onedir --noconfirm \
-            --contents-directory '$HOME/Desktop/pw-manager:.' \
-            --icon 'ding.icns' \
-            --windowed
-            main.py
--- setup.py
+++
@@ -1,3 +0,0 @@
-from setuptools import setup
-
-app = ['main.py']
